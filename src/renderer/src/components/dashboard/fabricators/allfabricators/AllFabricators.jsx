83941caa1621810b4503cc9072c5578560474503
raunak234362 <raunakdob@gmail.com>
2024-08-09 14:50:01 +0530

    chore: Update fabricator search functionality
    Refactor the AllFabricators component to include a search functionality for filtering fabricators by name. This feature allows users to search for fabricators by entering a name in the search input field. The fabricators list will be dynamically updated to display only the fabricators that match the search term.
    
    This change improves the user experience by providing a convenient way to find specific fabricators in the list.

--- src/renderer/src/components/dashboard/fabricators/allfabricators/AllFabricators.jsx
+++ src/renderer/src/components/dashboard/fabricators/allfabricators/AllFabricators.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable prettier/prettier */
 import React, { useEffect, useState } from "react";
 import Service from "../../../../api/configAPI";
 import { Header, Button, ManageFabricator } from "../../../index";
@@ -5,7 +7,9 @@ import { Header, Button, ManageFabricator } from "../../../index";
 const AllFabricators = () => {
 const [fabricators, setFabricators] = useState([]);
 const [selectedFabricator, setSelectedFabricator] = useState(null);
+const [filteredFab, setFilteredFab] = useState([]);
 const [isModalOpen, setIsModalOpen] = useState(false);
+const [searchTerm, setSearchTerm] = useState('');
 
 useEffect((token) => {
 const fetchFabricators = async () => {
@@ -20,6 +24,13 @@ const fetchFabricators = async () => {
 
 fetchFabricators();
 }, []);
+
+useEffect(() => {
+  const results = fabricators.filter(fab =>
+    fab.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  setFilteredFab(results);
+}, [searchTerm, fabricators]);
 
 const handleViewClick = async (fabricatorId) => {
     
@@ -40,6 +51,16 @@ const handleViewClick = async (fabricatorId) => {
 return (
 <div>
     <Header title={"All Fabricator"} />
+    <div className="flex justify-between mt-4">
+              <input
+                type="text"
+                placeholder="Search by Fabricator name"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="p-2 border border-gray-300 rounded-lg"
+              />
+             
+            </div>
     <table className="mt-10 min-w-full divide-y divide-gray-200">
     <thead className="bg-slate-200">
         <tr>
@@ -59,25 +80,12 @@ return (
         </tr>
     </thead>
     <tbody className="bg-white divide-y divide-gray-200">
-        {fabricators?.map((fabricator, index) => (
+        {filteredFab?.map((fabricator, index) => (
         <tr key={index}>
             <td className="px-6 py-4 whitespace-nowrap">{index + 1}</td>
             <td className="px-6 py-4 whitespace-nowrap">
             {fabricator?.name}
             </td>
-
-            {/* <td className="px-6 py-4 whitespace-nowrap">
-            {fabricator?.country}
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-            {fabricator?.state}
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-            {fabricator?.city}
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-            {fabricator?.zipCode}
-            </td> */}
             <td className="px-20 py-4 whitespace-nowrap">
             {fabricator?.connections}
             </td>
