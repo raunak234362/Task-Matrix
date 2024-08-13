30326082d92552e8ee1bf9e62da6e87944cafa37
raunak234362 <raunakdob@gmail.com>
2024-08-13 13:50:20 +0530

    chore: Update npm dependencies
    Update react-router-dom to version 6.26.0 and add update-electron-app dependency at version 3.0.0. This ensures that the project is using the latest versions of these packages, which may include bug fixes and new features.

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,9 @@
+/* eslint-disable prettier/prettier */
 import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
+import { updateElectronApp } from 'update-electron-app'
 
 function createWindow() {
   // Create the browser window.
@@ -69,6 +71,8 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+updateElectronApp()
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
