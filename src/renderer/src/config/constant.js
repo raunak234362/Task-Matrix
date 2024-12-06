bef1724749bb363e25f7910ca5a4490579e17e08
Raunak Deep Srivastava <raunakdob@gmail.com>
2024-12-06 15:14:07 +0530

    Update constant.js

--- src/renderer/src/config/constant.js
+++ src/renderer/src/config/constant.js
@@ -1,3 +1,3 @@
-export const BASE_URL='http://192.168.1.158:5152/'
+export const BASE_URL='http://192.168.1.152:5152/'
 // export const BASE_URL='http://192.168.1.49:8000/'
 // export const BASE_URL='https://projhub1.onrender.com/'
