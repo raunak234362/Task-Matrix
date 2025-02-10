0af22801d8a3ab8d52b91348b3c9c1dc54c6d85b
raunak234362 <raunakdob@gmail.com>
2025-02-10 13:35:43 +0530

    fix: update API baseURL to use local development server

--- src/renderer/src/api/api.js
+++ src/renderer/src/api/api.js
@@ -3,10 +3,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://106.51.141.125:5154",
+  // baseURL: "https://106.51.141.125:5154",
   // baseURL: "https://backend.whiteboardtec.com:5154",
   // baseURL: "https://whiteboardtec.com/backend",
-  // baseURL: "https://192.168.1.153:5154",
+  baseURL: "https://192.168.1.153:5154",
   // baseURL: "http://192.168.1.153:3000",
   // baseURL: 'https://projectstationbe.onrender.com/',
   withCredentials: false, // Set to false to avoid CORS preflight
