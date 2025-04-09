f6b582cf8ab7e62956523b051abc664db14c9e05
raunak234362 <raunakdob@gmail.com>
2025-04-09 14:09:10 +0530

    refactor: remove commented-out sidebar toggle code in Header component

--- src/renderer/src/components/dashboard/Header.jsx
+++ src/renderer/src/components/dashboard/Header.jsx
@@ -26,10 +26,7 @@ const Header = ({title}) => {
   return (
     <div className="flex items-center justify-between w-full bg-white bg-opacity-50 border-4 md:px-5 rounded-xl text-slate-800">
       <div className={`flex items-center p-1 transition-all duration-300 `}>
-        {/* <div
-          className={`fixed inset-0 z-10 transition-opacity duration-300 `}
-          onClick={toggleSidebar}
-        ></div> */}
+       
         <img src={LOGO} alt="" className="w-20 md:w-32" />
       </div>
       <h1 className="mx-auto text-2xl">{title}</h1>
