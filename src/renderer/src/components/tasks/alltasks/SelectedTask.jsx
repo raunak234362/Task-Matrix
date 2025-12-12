72a2fe1829a1b5825178474147224ba0566887c5
Raunak <raunakdob@gmail.com>
2025-12-12 12:15:00 +0530

    feat: Temporarily disable start/end timing display in selected task details.

--- src/renderer/src/components/tasks/alltasks/SelectedTask.jsx
+++ src/renderer/src/components/tasks/alltasks/SelectedTask.jsx
@@ -362,7 +362,7 @@ const SelectedTask = ({ taskDetail, taskId, isOpen, onClose, setTasks }) => {
                           )}
                         </p>
                       </div>
-                      <div>
+                      {/* <div>
                         <p className="text-sm font-medium text-gray-500">
                           Start Timing
                         </p>
@@ -385,7 +385,7 @@ const SelectedTask = ({ taskDetail, taskId, isOpen, onClose, setTasks }) => {
                             )?.end,
                           )}
                         </p>
-                      </div>
+                      </div> */}
                     </div>
                     <div className="flex items-start gap-3">
                       <Clock className="w-5 h-5 mt-0.5 text-gray-500" />
